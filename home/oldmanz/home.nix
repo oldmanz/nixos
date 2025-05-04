2bf0536634075d8cc01b47fa38f27daada19fdc3
Travis Zimmerman <59665029+oldmanz@users.noreply.github.com>
2025-05-04 16:49:01 -0400

    Update home.nix

--- home/oldmanz/home.nix
+++ home/oldmanz/home.nix
@@ -9,8 +9,8 @@
   z.home.impermanence.enable = mkIf (z.system.impermanence.enable == true) true;false;
   z.home.git.enable = true;
   z.home.zsh.enable = true;
-  z.home.gnome.enable = true;
-  z.home.hyprland.enable = false;
+  z.home.gnome.enable = mkIf (z.system.display.gnome.enable == true) true;false;
+  z.home.hyprland.enable = mkIf (z.system.display.hyprland.enable == true) true;false;
 
   home.stateVersion = "24.05";
 
