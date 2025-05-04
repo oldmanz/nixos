e508b7cd757dbfef7aeeb45a2d4a78a00ee50fd9
oldmanz <travis@oldmanz.com>
2025-05-04 07:33:03 -0400

    t

---
+++ modules/system/display/kde.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, inputs, ... }: {
+
+    options = {
+    z.system.display.kde.enable = 
+        lib.mkEnableOption "enables kde";
+    };
+
+    config = lib.mkIf config.z.system.display.kde.enable {
+      services.xserver.enable = true;
+      services.displayManager.sddm.enable = true;
+      services.desktopManager.plasma6.enable = true;
+    };
+}
